caa7849f969aa4ad0545613774764137e525a3ec
milakai <freezedlake@gmail.com>
2022-02-28 19:53:16 +0100

    commit prob liste

--- Permutations.py
+++ Permutations.py
@@ -1,23 +1,22 @@
 class Cube:
     index=['S','I','B','W']                 #stores name of cubes
-    sides=["A1","A2","A3","A4","A5","A6"]   #stores name of sides
+    sides=["A1","A2","S3","A4","A5","A6"]   #stores name of sides
     position=0                              #stores position of cubes
-    def __init__(self, index, sides, position):
+    def __init__(self, index): #sides, position
         self.index=index
-        self.sides=sides
-        self.position=position
+        # self.sides=sides
+        # self.position=position
 
 class CubeConstruct:
     shape=["XYZW"]                                     #stores the cube order
-    side=[0,0,0,0,0,0]
+    side=[0,0,0,0,0,1]
     sides=[side,side,side,side]
-    figure={"F000":[sides]}     #stores the contacts between cubes
+    figure={"F000":[sides]}                             #stores the contacts between cubes
                                                         #following the decomposed view of a cube
     fig_name=" "
     
-    def __init__(self, shape, figure):
+    def __init__(self, shape):
         self.shape=shape
-        self.figure=figure
         
     def stable(self):
         while_repeat=True                           #à modifier avec la valeuer du stable_prompt?
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-import Permutations
+import Permutations as p
 
-###Init cubes
-Sensor=Cube('S')
-Button=Cube('B')
-Inverter=Cube('I')
-Wheels=Cube('W')
+####find syntax to manipulate figure Var, start by trying /w one list 28/02
+
+###Init cubes/variables
+Sensor=p.Cube('S')     #index[1]
+Button=p.Cube('B')      #index[2]
+Inverter=p.Cube('I')     #index[3]   
+Wheels=p.Cube('W')      #index[4]
+nomsFigure=["F000","F002","F010","F011","F012","F013","F014","F015","F020","F021","F022","F024","F025","F030","F034","F042","F044","F045","F050","F051","F060","F061"]
+cubeOrder = ["BISW","BIWS","BSIW","BSWI","BWSI","IBSW","IBWS","BWIS","ISBW","ISWB","IWBS","IWSB","SBIW","SBWI","SIBW","SWBI","SWIB","SIWB","WBIS","WBSI","WIBS","WISB","WSBI","WSIB"]
+#CubeTemplate = p.Cube(cubeOrder[1])
+
 ###Init figure
-Fxxx=CubeConstruct()
-for i in range(len(Fxxx.figure{"F000":Fxxx.sides[Fxxx.side[i],Fxxx.side[i],Fxxx.side[i],Fxxx.side[i]]})):
-    Fxxx.figure{}=
+Fxxx=p.CubeConstruct(cubeOrder[1])
+for i in range(len(nomsFigure)):
+    Fxxx.figure.update({nomsFigure[i]:Fxxx.figure})
+    
+###Permutation logic algorithm
+for i in range(6):      #i,j,k,l,n
+    #for j in range(6):
+        #for k in range()
+    if Fxxx.figure["F000":Fxxx.sides[Fxxx.side[i]]]==1:
+        Fxxx.figure["F000":Fxxx.sides[Fxxx.side]]=1
+        
 
-obj1=CubeConstruct()
+print(Fxxx.figure)        #use object cubes to innitialise .figure to actual cubes  
+#obj1=CubeConstruct()
